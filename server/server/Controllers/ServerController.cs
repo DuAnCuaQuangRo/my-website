7f78c85694438defa90904773b9e4f9877e37de5
DESKTOP-VNCP4C0\THUY-PC <thuytwandpro@gmail.com>
2018-05-25 16:31:21 +0700

    d
    d

---
+++ server/server/Controllers/ServerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace server.Controllers
+{
+    public class ServerController : Controller
+    {
+        // GET: Server -> Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Server
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Server Information
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+        // GET: Server Admin Manager
+        public ActionResult Users()
+        {
+            return View();
+        }
+    }
+}
